587b6ac77cd802571070772fba26a1c9c9030072
tang-guihua <tl301@126.COM>
2017-03-05 20:05:30 +0800

    未完成

--- javascript/a.js
+++ javascript/a.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 	$(".sun").bind("click", function(){
 		$(".main").addClass("hide"),
 			setTimeout(function(){
-			$(".zong").addClass("show");
+			$(".zong").addClass("show"),
+			$(".suntext").addClass("show"),
+			$(".sidebar").addClass("show");
 		}, 500)
 	});
 });
