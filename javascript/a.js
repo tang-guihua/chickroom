c78e3d0e157dea4c915ada35785d9f5f4dcae567
tang-guihua <tl301@126.com>
2017-03-16 16:30:02 +0800

    加动态

--- javascript/a.js
+++ javascript/a.js
@@ -7,4 +7,30 @@ $(document).ready(function(){
 			$(".sidebar").addClass("show");
 		}, 500)
 	});
+	$('.aabb').mouseover(function(){
+		var that=$(this).next();
+		that.css('opacity','1');
+	});
+	$('.aabb').mouseout(function(){
+		var that=$(this).next();
+		that.css('opacity','0');
+	});
+	$('#shuMing-3').mouseover(function(){
+		$('.shuoMing-c').css({'opacity':'1','top':'60%'});
+	});
+	$('#shuMing-3').mouseout(function(){
+		$('.shuoMing-c').css('opacity','0');
+	});
+	$('#shuMing-4').mouseover(function(){
+		$('.shuoMing-d').css({'opacity':'1','top':'70%'});
+	});
+	$('#shuMing-4').mouseout(function(){
+		$('.shuoMing-d').css('opacity','0');
+	});
+	$('#shuMing-5').mouseover(function(){
+		$('.shuoMing-e').css({'opacity':'1','top':'35%'});
+	});
+	$('#shuMing-5').mouseout(function(){
+		$('.shuoMing-e').css('opacity','0');
+	});
 });
